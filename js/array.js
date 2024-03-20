7c62445f22feb8d1b589f9191d23f42f1a4be9f7
edilson gomes de lima <edskateicevida@gmail.com>
2024-03-20 15:22:47 -0300

    programação javscript Array

---
+++ js/array.js
@@ -0,0 +1,16 @@
+var avgTemp = []
+
+avgTemp[0] = 31.9
+avgTemp[1] = 35.3
+avgTemp[2] = 42
+avgTemp[3] = 38
+avgTemp[4] = 25.5
+
+console.log(avgTemp[3])
+
+
+var daysOfweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+
+
+console.log(daysOfweek[3])
